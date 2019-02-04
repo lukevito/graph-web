09588ff9f5e9c1ef4fa2162800151dc57eeb5178
Kuras <lukevito@gmail.com>
2019-02-04 19:43:42 +0100

    5:)

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import {Component, NgModule} from '@angular/core';
+import {Component} from '@angular/core';
 import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
 import {Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
